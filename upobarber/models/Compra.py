c466001ae3507d0579e2d71018cb3a55d29594ab
joseantonio_or <48123924+orozcorodrijoseantonio@users.noreply.github.com>
2023-12-06 12:19:00 +0100

    Modificacion 003
    Hecho también en en repositorio Segunda Entrega.
    Están hechos depends, constraints, sql_constrains.
    Y también los reports(sale arriba Estados Unidos, solucionar eso).
    Falta OWL.

---
+++ upobarber/models/Compra.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from datetime import datetime
+
+class Compra(models.Model):
+    _name = 'upobarber.compra'
+    _description = 'upobarber Compra'
+
+    name = fields.Char(string="idCompra", index=True, required=True)
+    importe = fields.Float(string="Importe", required=True)
+    fechaCompra = fields.Datetime(
+        string='Fecha de la compra',
+        widget='datetime',
+        options={'format': 'YYYY-MM-DD HH:mm:ss'},
+    )
+
+    pago_id = fields.Many2one('upobarber.pago', string="Concepto del pago")
+    #cliente_id = fields.Many2one('upobarber.cliente', string="Cliente")
+    #articulo_id = fields.One2many('upobarber.articulo', 'compra_id', "Compra")
+
+    @api.constrains('importe')
+    def _check_importe_valido(self):
+        if self.importe <= 0:
+            raise models.ValidationError('El importe debe ser un importe válido!!')
+     
+    @api.constrains('fechaCompra')
+    def _chech_fecha_valida(self):
+        if self.fechaCompra > datetime.now():
+            raise models.ValidationError('La fecha debe ser una fecha válida!! No puede ser una fecha más tarde de la actual')
+
+    sql_constraints = [
+        ('name_uniq',
+        'UNIQUE (name)',
+        'El id de la compra debe ser único.')
+    ]
